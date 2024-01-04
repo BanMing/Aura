b9ad98e158bf5208d8a074a4d300e50b95e50c2f
BanMing <ban-ming@foxmail.com>
2024-01-04 23:34:45 +0800

    Add User Widget

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,9 @@
+// Copyright BanMing
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
