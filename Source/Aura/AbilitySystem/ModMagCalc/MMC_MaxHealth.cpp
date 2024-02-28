1b454a5713150068f3662cf6716c367f4d47cc00
BanMing <wupeng_a2484@virtuos.com.cn>
2024-02-28 19:17:52 +0800

    add mmc  max health

---
+++ Source/Aura/AbilitySystem/ModMagCalc/MMC_MaxHealth.cpp
@@ -0,0 +1,7 @@
+// Copyright BanMing
+
+#include "Aura/AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth()
+{
+}
---
+++ Source/Aura/AbilitySystem/ModMagCalc/MMC_MaxHealth.h
@@ -0,0 +1,21 @@
+// Copyright BanMing
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+
+#include "MMC_MaxHealth.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UMMC_MaxHealth : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UMMC_MaxHealth();
+
+};
